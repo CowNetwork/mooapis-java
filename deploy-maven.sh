16a282d7d0a35d9fa212cef6a06f956a7c47b3e4
Tobias B <germansuperioz@yahoo.de>
2021-04-08 11:05:06 +0200

    Add deploy script

--- deploy-maven.sh
+++ deploy-maven.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # loop through all folders to find pom.xml and deploy these projects
+for f in $(find "." -type f -name "pom.xml"); do
+    parentDir="$(dirname "$f")"
+
+    cd "$parentDir"
+    mvn deploy
+done
