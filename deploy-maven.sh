6d8a40bbd8bae401d3498bd9166166f37ea1cd5b
Benedikt W <wuelleronline@gmail.com>
2021-04-12 17:48:35 +0200

    Update deploy-maven.sh

--- deploy-maven.sh
+++ deploy-maven.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-PWD=$(pwd)
+BASE_DIR=$(pwd)
 
 # loop through all folders to find pom.xml and deploy these projects
 for f in $(find "." -type f -name "pom.xml"); do
-    cd "${PWD}"
-    echo "$(pwd)"
     echo "Building ${f}"
     parentDir="$(dirname "$f")"
 
     cd "$parentDir"
     mvn deploy
+    cd "${BASE_DIR}"
 done
