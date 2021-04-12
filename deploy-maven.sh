696fb276b692df85483e6d798a6a91726c55ae2b
Benedikt W <wuelleronline@gmail.com>
2021-04-12 17:38:50 +0200

    Update deploy-maven.sh

--- deploy-maven.sh
+++ deploy-maven.sh
@@ -4,6 +4,7 @@ PWD=$(pwd)
 
 # loop through all folders to find pom.xml and deploy these projects
 for f in $(find "." -type f -name "pom.xml"); do
+    echo "Building ${f}"
     parentDir="$(dirname "$f")"
 
     cd "$parentDir"
